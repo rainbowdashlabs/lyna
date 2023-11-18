8402edbe9a4b8cfd15b0635fce13b851f5159815
Lilly Tempest <46890129+rainbowdashlabs@users.noreply.github.com>
2023-11-18 21:56:04 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(18))
+        languageVersion.set(JavaLanguageVersion.of(21))
     }
     withSourcesJar()
     withJavadocJar()
